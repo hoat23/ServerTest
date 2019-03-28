94b5da364a70261e07fb017e7bbc9f2d35917f93
Deiner Zapata <deinerzapata@gmail.com>
2019-03-28 12:44:04 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var connect = require('connect');
+var http = require('http');
+var debug = require('debug')('wh');
+var app = connect();
+var format = function (input) { return JSON.stringify(input, null, 2); };
+var port = 3009;
+
+// Parse JSON requests into `req.body`.
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Print all incoming requests, then respond to them.
+app.use(function (req, res) {
+    console.log("incomming webhook");
+    debug('incoming webhook', req.method, req.url, format(req.body));
+    res.end('OK\n');
+});
+
+// create node.js http server and listen on port 3009
+var server = http.createServer(app);
+server.on('error', function (err) {
+    console.log("error")
+    debug('server error', err);
+});
+server.listen(port, function () {
+    console.log("respond webhook server...")
+    debug('Respoke webhook server is listening on port', port);
+});
+
