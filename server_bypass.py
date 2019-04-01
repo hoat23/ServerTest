3e7b8735c312d5360669bac2abd4e7f084a1f614
Deiner Zapata <deinerzapata@gmail.com>
2019-04-01 13:03:11 -0500

    Add files via upload

---
+++ server_bypass.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+# Developer: Deiner Zapata Silva.
+# Date: 01/14/2019
+# Description: Server to show everything to received.
+#########################################################################################
+import sys, requests, json, ast
+from utils import print_json
+from datetime import datetime, timedelta
+from flask import Flask, request, abort
+#######################################################################################
+app = Flask(__name__)
+#######################################################################################
+def req_get(URL_API, data=None, timeout=None):
+    try:
+        headers =  {'Content-Type': 'application/json'}
+        rpt = requests.get( url=URL_API, data=data, headers=headers, timeout=timeout)
+        print("{0} [INFO ] req_get |{1}|{2}|{3}|".format( datetime.datetime.utcnow().isoformat(), rpt.status_code, rpt.reason, URL_API))
+        print(rpt.text)
+        return rpt.text
+    except:
+        print("{0} [ERROR] req_get |{1}|{2}|{3}|".format( datetime.datetime.utcnow().isoformat(), rpt.status_code, rpt.reason, URL_API))
+        return ""
+#######################################################################################
+def req_post(URL_API, data=None, timeout=None):
+    try:
+        headers =  {'Content-Type': 'application/json'}
+        rpt = requests.post( url=URL_API, data=None, headers=headers , timeout=timeout)
+        print("{0} [INFO ] req_post|{1}|{2}|{3}|".format( datetime.datetime.utcnow().isoformat(), rpt.status_code, rpt.reason, URL_API))
+        print(rpt.text)
+        return rpt.text
+    except:
+        print("{0} [ERROR] req_post|{1}|{2}|{3}|".format( datetime.datetime.utcnow().isoformat(), rpt.status_code, rpt.reason, URL_API))
+        return ""
+#######################################################################################
+def bytesELK2json(data,codification='utf-8'):
+    d_dict = {}
+    try:
+       d_str = data.decode(codification)
+       d_str = d_str.replace("false","False")
+       d_dict = eval(d_str)
+    except:
+       print("[ERROR] type={0} ".format( type(data) ))
+    finally:
+       return d_dict
+#######################################################################################
+@app.route('/', methods=['POST'])
+def webhook_post():
+    URL = "http://1855b969.ngrook.io"
+    print("webhook_post()-> "+ str(sys.stdout.flush()) )
+    print_json( bytesELK2json( request.data ))
+    rpt = req_post(URL, data = request.data, timeout=None)
+    return '', 200
+#######################################################################################
+@app.route('/', methods=['GET'])
+def webhook_get():
+    URL = "http://1855b969.ngrook.io"
+    print("webhook_get()-> "+ str(sys.stdout.flush()) )
+    print_json( bytesELK2json( request.data ))
+    rpt = req_get(URL, data = request.data, timeout=None)
+    return '', 200
+#######################################################################################
+@app.route('/hearbeat_ping_down', methods=['GET','POST'])
+def solve_ping():
+    print("DEF ping-----------------------")
+    print( request.data )
+    return '', 200
+#######################################################################################
+if __name__ == '__main__':
+    app.run(host='172.30.0.114',port=3009)
