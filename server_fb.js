60d4cbe5ee5faad40854d067b94251b2a4070cff
Deiner Zapata <deinerzapata@gmail.com>
2019-04-02 12:19:31 -0500

    Update server_fb.js

--- server_fb.js
+++ server_fb.js
@@ -1,25 +1,116 @@
 'use strict';
+
 //Import dependencias and set up http server
 const 
     express = require('express'),
     bodyParser = require('body-parser'),
+    crypto = require('crypto'),
+    https = require('https'),
     app = express().use(bodyParser.json()); //Create express http server
 
+//Adding certificates LINK= https://sg.com.mx/revista/53/desarrollo-chatbots-para-facebook-messenger
+var fs = require('fs');
+/*
+var options = {
+    key : fs.redFileSync('/path/privKey.pem'),
+    cert : fs.readFileSync('/path/cert.pem'),
+    ca: fs.readFileSync
+}
+*/
 //Set server port an logs message on success
 app.listen(process.env.PORT || 4006, ()=> console.log('[INFO] webhook facebook listening'));
-
-
+/***************************************************************************************************************************************/
+/*                                                         HANDLES MESSAGES EVENTS                                                      /
+/***************************************************************************************************************************************/
+function handleMessage(sender_psid, received_message ) {
+    console.log("%s| handleMessage",datetime.toISOString());
+    let response;
+    //Check if the message contains text
+    if(received_message.text) {
+        //Create the payload for a basic text message
+        response = {
+            "text": 'Hola "{received_message.text}".'
+        }
+    }else if(received_message.attachments){
+        //Get url of the message attachmednt
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+          }
+    } 
+    //Sends the response message
+    callSendAPI(sender_psid, response);
+}
+/***************************************************************************************************************************************/
+function handlePostBack(sender_psid, received_postback) {
+    console.log("%s|shandlePostBack", datetime.toISOString());
+    let response;
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+}
+/***************************************************************************************************************************************/
+function callSendAPI(sender_psid, response) {
+    console.log("%s|callSenderAPI",datetime.toISOString());
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+    console.log(request_body);
+}
+/***************************************************************************************************************************************/
 app.post('/webhook', (req, res)=>{
     let datetime = new Date();
     let body = req.body;
     //Checks this is an event from a page subscription
     if (body.object == 'page') {
         //Iterates over each entry - there may be multiple if batched
-        body.entry.array.forEach( function(entry){
+        body.entry.forEach( function(entry){ //body.entry.array.forEach( function(entry){
             //Getting the message. entry.messaging is an array, but
             //will only contain one message, so we get index 0
             let webhook_event = entry.messaging[0];
             console.info("webhook_event = [%s]", webhook_event);
+            //Get the sender PSID 
+            let sender_psid = webhook_event.sender.id;
+            console.info("%s|INFO | webhook_event| PSID:%s", datetime.toISOString(), sender_psid);
+            //Chek if the event is a message or postback and pass the eventto aprropiate handler function
+            if(webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            }else if(webhook_event.postback) {
+                handlePostBack(sender_psid, webhook_event.postback);
+            }
+
         });
         res.status(200).send('EVENT_RECEIVED');
     }else{
@@ -27,7 +118,7 @@ app.post('/webhook', (req, res)=>{
         res.sendStatus(404);
     }
 });
-
+/***************************************************************************************************************************************/
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
     // Your verify token. Should be a random string.
