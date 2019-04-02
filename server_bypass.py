ba2b6bc1ae0b293e0ea76d7eb8da0b1b7cf0b047
Deiner Zapata <deinerzapata@gmail.com>
2019-04-02 16:43:09 -0500

    Update server_bypass.py

--- server_bypass.py
+++ server_bypass.py
@@ -9,7 +9,7 @@ from utils import print_json
 from datetime import datetime, timedelta
 from flask import Flask, request, abort
 #######################################################################################
-URL = "http://209fe4c7.ngrok.io"
+URL_NGROK = "http://209fe4c7.ngrok.io"
 app = Flask(__name__)
 #######################################################################################
 def req_get(URL_API, data=None, timeout=None):
@@ -26,7 +26,7 @@ def req_get(URL_API, data=None, timeout=None):
 def req_post(URL_API, data=None, timeout=None):
     try:
         headers =  {'Content-Type': 'application/json'}
-        rpt = requests.post( url=URL_API, data=None, headers=headers , timeout=timeout)
+        rpt = requests.post( url=URL_API, data=data, headers=headers , timeout=timeout)
         print("{0} [INFO ] req_post|{1}|{2}|{3}|".format( datetime.utcnow().isoformat(), rpt.status_code, rpt.reason, URL_API))
         print(rpt.text)
         return rpt.text
@@ -36,10 +36,13 @@ def req_post(URL_API, data=None, timeout=None):
 #######################################################################################
 def bytesELK2json(data,codification='utf-8'):
     d_dict = {}
+    if (type(data)==bytes):
+        print("[INFO] bytesELK2json | decoding <{0}:{1}>".format(len(data),codification))
+        data = data.decode(codification)
+
     try:
-       d_str = data.decode(codification)
-       d_str = d_str.replace("false","False")
-       d_dict = eval(d_str)
+       data = data.replace("false","False")
+       d_dict = eval(data)
     except:
        print("[ERROR] type={0} ".format( type(data) ))
     finally:
@@ -50,15 +53,14 @@ def webhook_post():
     #URL = "http://1855b969.ngrok.io"
     print("webhook_post()-> "+ str(sys.stdout.flush()) )
     print_json( bytesELK2json( request.data ))
-    rpt = req_post(URL, data = request.data, timeout=None)
+    rpt = req_post(URL_NGROK, data = request.data, timeout=None)
     return '', 200
 #######################################################################################
 @app.route('/', methods=['GET'])
 def webhook_get():
-    #URL = "http://1855b969.ngrok.io"
     print("webhook_get()-> "+ str(sys.stdout.flush()) )
     print_json( bytesELK2json( request.data ))
-    rpt = req_get(URL, data = request.data, timeout=None)
+    rpt = req_get(URL_NGROK, data = request.data, timeout=None)
     return '', 200
 #######################################################################################
 @app.route('/hearbeat_ping_down', methods=['GET','POST'])
@@ -68,4 +70,4 @@ def solve_ping():
     return '', 200
 #######################################################################################
 if __name__ == '__main__':
-    app.run(host='172.30.0.114',port=3009)
+    app.run(host='172.30.0.114',port=8080)
